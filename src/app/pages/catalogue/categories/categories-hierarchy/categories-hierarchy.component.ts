1f67a05ca37c187d2e3c5ae76f70f401cc582edc
IvankinaMarina <m.ivankina@vironit.co.uk>
2019-07-09 13:25:07 +0300

    added category hierarchy tree

--- src/app/pages/catalogue/categories/categories-hierarchy/categories-hierarchy.component.ts
+++ src/app/pages/catalogue/categories/categories-hierarchy/categories-hierarchy.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../services/category.service';
 
 @Component({
   selector: 'ngx-categories-hierarchy',
@@ -7,10 +8,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriesHierarchyComponent implements OnInit {
 
-  constructor() {
+  categories = [];
+  myTree = [];
+
+  config = {
+    showActionButtons: false,
+    showRenameButtons: false,
+    showDeleteButtons: false,
+    showRootActionButtons: false,
+    enableExpandButtons: true,
+    enableDragging: true,
+    rootTitle: 'Categories hierarchy',
+    validationText: '',
+    minCharacterLength: 5,
+    setItemsAsLinks: false,
+    setFontSize: 32,
+    setIconSize: 16
+  };
+
+  constructor(
+    private categoryService: CategoryService,
+    ) {
   }
 
   ngOnInit() {
+    this.getList();
+  }
+
+  getList() {
+    this.categoryService.getListOfCategories()
+      .subscribe(categories => {
+        categories.forEach((el) => {
+          this.transformList(el);
+        });
+        this.myTree = [...categories];
+      });
+  }
+
+  transformList(node) {
+    node.name = node.description.name;
+    node.childrens = [...node.children];
+    if (node.children && node.children.length !== 0) {
+      node.children.forEach((el) => {
+        this.transformList(el);
+      });
+    }
+  }
+
+  onDrop(event) {
+    console.log(event);
   }
 
 }
--- src/app/pages/catalogue/categories/categories.module.ts
+++ src/app/pages/catalogue/categories/categories.module.ts
@@ -12,6 +12,7 @@ import { CategoryDetailComponent } from './category-detail/category-detail.compo
 import { NgxSummernoteModule } from 'ngx-summernote';
 import { NbDialogModule } from '@nebular/theme';
 import { ShowcaseDialogComponent } from './categories-list/showcase-dialog/showcase-dialog.component';
+import { NgxTreeDndModule } from 'ngx-tree-dnd';
 
 
 @NgModule({
@@ -32,6 +33,7 @@ import { ShowcaseDialogComponent } from './categories-list/showcase-dialog/showc
 
     NgxSummernoteModule,
     NbDialogModule.forChild(),
+    NgxTreeDndModule,
   ],
   entryComponents: [ButtonRenderComponent, ShowcaseDialogComponent],
 })
